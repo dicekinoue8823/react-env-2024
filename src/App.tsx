8a96b5b58e59bfb65355e020d86203077a61f967
dicek8823 <dicek8823@icloud.com>
2024-07-17 11:07:53 +0900

    1st commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import Button from "@/components/Button.tsx";
 const App = () => {
     return (
         <div>
-            <div className="max-w-4xl py-20 flex gap-4 mx-auto">
+            <div className="max-w-4xl flex justify-center gap-4 mx-auto">
                 <Button onClick={() => alert("Create")}>Button-1 </Button>
-                <Button>Button-2 </Button>
+                <Button className="bg-green-500 hover:bg-green-700">Button-2 </Button>
+                <Button variant="secondary">Edit</Button>
+                <Button variant="danger" size="sm" pending={true}>Delete</Button>
 
             </div>
         </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,55 @@
 import {ButtonHTMLAttributes} from "react";
+import {cva, VariantProps} from "class-variance-authority";
+import {cn} from "@/lib/utils.ts";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants>{
     children: React.ReactNode;
+    pending?: boolean;
 }
 
-const Button = ({ children, ...props }: ButtonProps) => {
+const buttonVariants =cva("rounded-md font-medium text-white shadow", {
+      variants: {
+          variant: {
+              primary: "bg-sky-500 hover:bg-sky-700",
+              secondary: "bg-orange-500 hover:bg-orange-700",
+              danger: "bg-red-500 hover:bg-red-700",
+          },
+          size:{
+              sm: "text-sm px-4 py-2",
+              md: "text-base px-6 py-4",
+              lg: "text-xl px-8 py-10",
+
+          },
+      },
+    defaultVariants: {
+          variant: "primary",
+          size: "md",
+    }
+});
+
+const Button = ({
+                    children,
+                    className,
+                    variant,
+                    size,
+                    pending,
+                    ...props
+}: ButtonProps) => {
     return (
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                {...props}>
-            { children }
+        // <button className={twMerge("bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-all", className)}
+        //         {...props}>
+        //     { children }
+        // </button>
+        <button className={
+          cn(buttonVariants({ variant, size, className }),{
+          "bg-slate-900": pending,
+          })}
+        {...props}
+        >
+            {children}
         </button>
-    );
+    )
 }
 
 export default Button;
